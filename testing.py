233b8dcf7974f9874c64f1fbe31f4cfb37124520
trrt-good <t.atwood.rose@gmail.com>
2023-11-15 16:49:50 -0800

    cleanup

--- testing.py
+++ testing.py
@@ -1,14 +1,2 @@
 import torch
-import numpy as np
-
-np.set_printoptions(threshold=np.inf, linewidth=np.inf)
-
-data=torch.load(r"C:\Users\tatwo\Downloads\encoded_data_lhs")
-
-print(data[0])
-
-# with open('data/output.txt', 'w') as f:
-#     for j in range(50):
-#         for i in data[j]:
-#             f.write(j)
-#         f.write("\n")  # If you want a newline after each outer loop iteration
+print(torch.__version__)
